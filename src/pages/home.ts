be0f42b69c4aecd81a833db832382b3ae0ee6b93
Gavin Sonntag <gsag@ucla.edu>
2026-02-06 02:29:53 -0800

    text changes and button styling

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -10,8 +10,9 @@ export function createHomePage(): string {
         <div class="divider"></div>
         <div class="hero-right">
           <h1>Tomas Moran</h1>
-          <h2 class="subtitle">Medill School of Journalism</h2>
-          <h3 class="subsubtitle">Northwestern University</h3>
+          <!--<h2 class="subtitle">Medill School of Journalism</h2>
+          <h3 class="subsubtitle">Northwestern University</h3>-->
+          <p>Hi, I'm Tomas Moran, a bilingual Northwestern student journalist! I have multimedia experience, including television, radio, print and social media. Storytelling in English and Spanish is my passion.</p>
           <div class="button-group">
             <a href="#work" class="btn btn-primary">My Work</a>
             <a href="#resume" class="btn btn-secondary">Resume</a>
