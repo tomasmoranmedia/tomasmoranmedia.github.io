537c3885fbdf88be9b826e1f3a91d79275bfc6c0
Tomas Moran <tomasjm333@gmail.com>
2026-02-27 02:54:07 -0600

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,7 +12,7 @@ function HomePage() {
         <div className="divider" />
         <div className="hero-right">
           <h1>Tomas Moran</h1>
-          <p>Hi, I'm Tomas Moran, a student journalist at Northwestern University! I have multimedia experience, including television, radio, print and social media. As a fully bilingual, biliterate and bicultural Peruvian American, reporting the facts through storytelling in English and Spanish is my passion.</p>
+          <p>Hi, I'm Tomas Moran, a student journalist at Northwestern University! I have multimedia experience; including television, radio, print and social media. As a fully bilingual, biliterate and bicultural Peruvian American, reporting the facts through storytelling in English and Spanish is my passion.</p>
           <div className="button-group">
             <Link to="/work" className="btn btn-primary">My Work</Link>
             <Link to="/resume" className="btn btn-secondary">Resume</Link>
