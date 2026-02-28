be40beed2dd4fde85540b1ba85d142d9b3a69240
Tomas Moran <tomasjm333@gmail.com>
2026-02-28 17:46:32 -0600

    Update television.tsx

--- src/pages/television.tsx
+++ src/pages/television.tsx
@@ -12,7 +12,7 @@ function TelevisionPage() {
   const studioItems = [
     {
       href: "https://drive.google.com/file/d/1JOeiImnmEFLfLfNq14pGVbyOYHfRJZnx/view?usp=sharing",
-      imageSrc: "/television/costa verde.heic",
+      imageSrc: "/television/costa_verde_reel.heic",
       imageAlt: "Lima Costa Verde",
       title: "\"SportsNight\" Keenan Fitzmorris in Peru Package (English)",
       date: "2/26/26"
