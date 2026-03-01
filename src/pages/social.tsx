92bafabfa3aa3781f6bbd53ad2acbe722b3bcf65
Tomas Moran <tomasjm333@gmail.com>
2026-02-28 18:14:13 -0600

    Update social.tsx

--- src/pages/social.tsx
+++ src/pages/social.tsx
@@ -14,7 +14,7 @@ function SocialMediaPage() {
       imageSrc: "/social/lima_cerro.heic",
       imageAlt: "Interview with Teresa Limon",
       title: "WNUR Sports Peru Road Trip Reel (English)"
-      date: "2/27/25"
+      date: "2/27/26"
     },
     {
       href: "https://www.instagram.com/reel/DRdkAIwjXZG",
