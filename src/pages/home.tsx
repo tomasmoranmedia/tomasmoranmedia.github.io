9a76337e86e786a25962f9819d03c27cff16c932
Tomas Moran <tomasjm333@gmail.com>
2026-08-17 00:03:58 -0400

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,7 +12,8 @@ function HomePage() {
         <div className="divider" />
         <div className="hero-right">
           <h1>Tomas Moran</h1>
-          <p>Hi, I'm Tomas Moran, a rising third-year student journalist at Northwestern University! Currently, I'm interning on The Lead with Jake Tapper at CNN's Washington, DC bureau. I have multimedia experience, including television, radio, print and social media. As a fully bilingual, biliterate and bicultural Peruvian American, reporting the facts through storytelling in English and Spanish is my passion.</p>
+          <p>Hi, I'm Tomas Moran, a rising third-year student journalist at Northwestern University! Currently, I'm interning on The Lead with Jake Tapper at CNN's Washington, DC bureau.
+            I have multimedia experience, including television, radio, print and social media. As a fully bilingual, biliterate and bicultural Peruvian American, reporting the facts through storytelling is my passion.</p>
           <div className="button-group">
             <Link to="/work" className="btn btn-primary">My Work</Link>
             <Link to="/resume" className="btn btn-secondary">Resume</Link>
